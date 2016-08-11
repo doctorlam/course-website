0595aa5323d11194207c722713aed2db5ad89a87
Christopher Lam <ChrisLam@Christophers-MacBook-Air.local>
2016-08-11 13:44:27 -0500

    Created Admin user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811174630) do
+ActiveRecord::Schema.define(version: 20160811182845) do
 
   create_table "add_attachment_to_lessons", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20160811174630) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
