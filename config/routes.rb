105a30ce13df72026f97b09f01666ddb334371da
Christopher Lam <ChrisLam@ChristophersAir.home>
2016-08-10 22:17:11 -0500

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :lessons
+root :to => 'lessons#index'  
+resources :lessons
   get 'pages/about'
 
   get 'pages/policies'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
