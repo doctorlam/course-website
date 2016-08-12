d7d8af6d241fe0966325e75916ecaf338cb1184f
Christopher Lam <ChrisLam@dynamic-10-125-158-205.dynamic.unt.edu>
2016-08-12 11:06:32 -0500

    added project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -77,7 +77,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :project_id, criterium_attributes: [:description, :id, :_destroy], deliverables_attributes: [:id, :description, :_destroy], related_attributes: [:description, :id, :_destroy])
+      params.require(:project).permit(:name, :description, :project_id, criterium_attribute: [:description, :id, :_destroy], deliverables_attributes: [:id, :description, :_destroy], related_attributes: [:description, :id, :_destroy])
     end
 
      def check_user
