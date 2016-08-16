c22a1ba90135cda4a14f850e0efba4453adc9a21
Christopher Lam <ChrisLam@ChristophersAir.home>
2016-08-16 06:55:15 -0500

    trying to fix default orde

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -21,5 +21,7 @@ validates_attachment :document, :content_type => { :content_type => %w(applicati
 	belongs_to :course
 	acts_as_commontable
 	acts_as_votable
+	default_scope order('created_at DESC')
+
 
 end
