d6cdc0c09d68be5818f58d98046916f2cc6a95ac
Christopher Lam <ChrisLam@dynamic-10-125-155-174.dynamic.unt.edu>
2016-08-17 13:21:16 -0500

    reorrdering date

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,6 @@
-Date::DATE_FORMATS[:default] = "%e %b %Y"
+Date::DATE_FORMATS[:default] = "%b %e %Y"
 Time::DATE_FORMATS[:default] = "%H:%M"
-DEFAULT_DATETIME_FORMAT = "%e %b %Y %H:%M"
+DEFAULT_DATETIME_FORMAT = "%b %e %Y %H:%M"
 class DateTime 
   def to_s 
     strftime DEFAULT_DATETIME_FORMAT
