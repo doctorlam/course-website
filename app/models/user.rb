d13687646a8b52323ef49a834fb411952570ad5a
Christopher Lam <ChrisLam@dynamic-10-125-150-120.dynamic.unt.edu>
2016-08-29 12:12:13 -0500

    added dependent destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :lessons, dependent: :destroy 
    has_many :projects, dependent: :destroy 
    has_many :abouts, dependent: :destroy
-   has_many :submissions
+   has_many :submissions, dependent: :destroy
 
    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
