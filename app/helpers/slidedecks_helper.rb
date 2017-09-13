5cece643b1e23f9ea581d21158426a24d3e530f0
Chris <chris.lam.unt@gmail.com>
2017-09-13 08:27:22 -0500

    tring to fix slide

--- app/helpers/slidedecks_helper.rb
+++ app/helpers/slidedecks_helper.rb
@@ -1,8 +1,6 @@
 module SlidedecksHelper
 	def show_slide_bg
     	  "background-color: #{@slide.bgcolor}; 
-    	  color: #{@slide.color_scheme};
-    	  min-height: 550px;
-    	  "
+    	  color: #{@slide.color_scheme};"
   	end
 end
