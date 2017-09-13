c7a6e3cbe9922ea4a41dfd967111b5fd05425d2c
Chris <chris.lam.unt@gmail.com>
2017-09-12 23:44:26 -0500

    a;dlfkj

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -9,6 +9,7 @@ class Slide < ActiveRecord::Base
   has_attached_file :bgimage, :styles => { :medium => "150x150>", :thumb => "30x30" }
   validates_attachment_content_type :bgimage, :content_type => /\Aimage\/.*\Z/
 
-  validates_presence_of :slide_order
+  validates :slide_order, :presence => true  #or you can also write as 'validates_presence_of :username'
+
 
 end
