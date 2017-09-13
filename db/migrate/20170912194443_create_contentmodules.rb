d3f107ee5a325b435d39d223dbfa8bf0a3708241
Chris <chris.lam.unt@gmail.com>
2017-09-12 22:31:41 -0500

    try again

--- db/migrate/20170912194443_create_contentmodules.rb
+++ db/migrate/20170912194443_create_contentmodules.rb
@@ -2,7 +2,7 @@ class CreateContentmodules < ActiveRecord::Migration
   def change
     create_table :contentmodules do |t|
       t.text :modular_content
-      t.belongs_to :slides, index: true, foreign_key: true
+      t.belongs_to :slide, index: true, foreign_key: true
 
       t.timestamps null: false
     end
