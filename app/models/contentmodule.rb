344f4a06d6fb05711807dd3cb6cfb669aa6028d6
Chris <chris.lam.unt@gmail.com>
2017-09-12 22:24:26 -0500

    removed migratin

--- app/models/contentmodule.rb
+++ app/models/contentmodule.rb
@@ -1,6 +1,4 @@
 class Contentmodule < ActiveRecord::Base
   belongs_to :slides
 
-   has_attached_file :modularimage, :styles => { :medium => "150x150>", :thumb => "30x30" }
-  validates_attachment_content_type :modularimage, :content_type => /\Aimage\/.*\Z/
 end
--- db/migrate/20170912195353_add_slide_id_to_contentmodule.rb
+++
@@ -1,5 +0,0 @@
-class AddSlideIdToContentmodule < ActiveRecord::Migration
-  def change
-    add_column :contentmodules, :slide_id, :integer
-  end
-end
