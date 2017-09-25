2075cbc1c933c28b4b921cfd78bbbf2af0841995
Chris <chris.lam.unt@gmail.com>
2017-09-25 06:33:50 -0500

    subnissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,11 +1,9 @@
 class SubmissionsController < ApplicationController
       before_action :set_submission, only: [:show, :edit, :update, :destroy]
       before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-      
-      before_filter :user_is_current_user, only: [:show, :edit, :update, :destroy]
-
       before_filter :check_user, only: [:edit, :update, :destroy]
       before_filter :authorize_admin, only: [:index, :destroy, :edit]
+      before_filter :user_is_current_user, only: [:show, :edit, :update, :destroy]
 
 
   # GET /submissions
