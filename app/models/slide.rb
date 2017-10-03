3e6443a0bf2385a7794679907f9e58f5545b7d6e
Chris <chris.lam.unt@gmail.com>
2017-10-03 15:57:12 -0500

    new styles for slides

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -8,7 +8,7 @@ class Slide < ActiveRecord::Base
 
 
   has_attached_file :image, :styles => { :medium => "150x150>", :thumb => "30x30" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+      do_not_validate_attachment_file_type :image
 
   has_attached_file :bgimage, :styles => { :medium => "150x150>", :thumb => "30x30" }
   validates_attachment_content_type :bgimage, :content_type => /\Aimage\/.*\Z/
