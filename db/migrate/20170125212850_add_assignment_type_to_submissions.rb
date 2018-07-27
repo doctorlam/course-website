850305da2c12dfbaec1594d3060f80ea5e9b9e0f
doctorlam <chris.lam.unt@gmail.com>
2018-07-27 11:36:16 -0500

    a;ldsfkj

--- db/migrate/20170125212850_add_assignment_type_to_submissions.rb
+++
@@ -1,5 +0,0 @@
-class AddAssignmentTypeToSubmissions < ActiveRecord::Migration
-  def change
-    add_column :submissions, :assignment_type, :string
-  end
-end
--- db/migrate/20170125213031_add_assignment_type_to_assignments.rb
+++
@@ -1,5 +0,0 @@
-class AddAssignmentTypeToAssignments < ActiveRecord::Migration
-  def change
-    add_column :assignments, :assignment_type, :string
-  end
-end
