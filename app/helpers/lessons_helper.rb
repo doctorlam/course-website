9de46877e23c4b1cfe578c289f6e4eb0d4239d41
doctorlam <chris.lam.unt@gmail.com>
2018-08-23 14:34:17 -0500

    fixing hide and show

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -5,4 +5,7 @@ module LessonsHelper
 	def options_for_week
 		['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5','Week 6', 'Week 7', 'Week 8','Week 9', 'Week 10','Week 11','Week 12','Week 13','Week 14','Week 15']
 	end
+	def truncate_with_hover(text_to_truncate, length = 30)
+    "<span title='#{text_to_truncate.gsub("'","\\'")}'>#{truncate(text_to_truncate, :length => length)}</span>" if !text_to_truncate.blank?
+end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
-Date::DATE_FORMATS[:default] = "%b %e"
+Date::DATE_FORMATS[:default] = "%b %e, %Y"
 Time::DATE_FORMATS[:default] = "%H:%M"
 DEFAULT_DATETIME_FORMAT = "%b %e %Y %H:%M"
 class DateTime 
